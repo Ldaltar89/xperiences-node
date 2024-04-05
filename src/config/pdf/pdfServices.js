fdd136193fe66a934732818f56222495d2314a15
Leonardo Altamirano <53161185+Ldaltar89@users.noreply.github.com>
2024-04-04 23:22:27 -0500

    feat(examen): implementacion y estructura de modulo examen

--- src/config/pdf/pdfServices.js
+++ src/config/pdf/pdfServices.js
@@ -1,26 +1,3 @@
-// const puppeteer = require("puppeteer");
-// const path = require("path");
-
-// const generatePDF = async (user, contractHTML) => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   // Reemplazar variables en el contrato HTML
-//   const processedContract = contractHTML
-//     .replace(/{name}/g, user ? user.name : "")
-//     .replace(/{dni}/g, user ? user.dni : "");
-
-//   await page.setContent(processedContract);
-
-//   // Generar PDF
-//   const pdfName = `contract_${user.name}_${user.dni}_${Date.now()}.pdf`;
-//   const pdfPath = path.join(__dirname, "pdfs", pdfName);
-//   await page.pdf({ path: pdfPath, format: "A4" });
-
-//   await browser.close();
-
-//   return pdfPath;
-// };
 
 const puppeteer = require("puppeteer");
 
